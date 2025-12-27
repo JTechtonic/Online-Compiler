99b590280ae0c699b77d0c121bd6417591a5552b
JTechtonic <jteix200396@gmail.com>
2025-12-26 22:11:18 -0500

    Finished API work
    Finished working on AWS API and added fetch call to fronted.

--- compiler-app-backend/lambda_function.py
+++ compiler-app-backend/lambda_function.py
@@ -4,95 +4,100 @@ import io
 
 
 def execute_python(code):
-    # Executes python code and captures output
-    original_stdout = sys.stdout
-    sys.stdout = output_capture = io.StringIO()
+	# Executes python code and captures output
+	original_stdout = sys.stdout
+	sys.stdout = output_capture = io.StringIO()
 
-    try:
-        exec(code) # Use exec() to capture output
-        output = output_capture.getvalue()
-        print('out of the code', output)
-        return output
-    except Exception as e:
-        return str(e)
-    finally:
-        sys.stdout = original_stdout
+	try:
+		exec(code) # Use exec() to capture output
+		output = output_capture.getvalue()
+		print('out of the code', output)
+		return output
+	except Exception as e:
+		return str(e)
+	finally:
+		sys.stdout = original_stdout
 
 
 def execute_java(code):
-    try:
-        print('Received code: ', code)
-        # Create a temp java file
-        with open('/tmp/Main.java', 'w') as java_file:
-            java_file.write(code)
+	try:
+		print('Received code: ', code)
+		# Create a temp java file
+		with open('/tmp/Main.java', 'w') as java_file:
+			java_file.write(code)
 
-        # Compile the Java source file
-        compiler_result = subprocess.run(
-            ['javac', '/tmp/Main.java'], stdout=subprocess.PIPE, stderr=subprocess.PIPE
-        )
+		# Compile the Java source file
+		compiler_result = subprocess.run(
+			['javac', '/tmp/Main.java'], stdout=subprocess.PIPE, stderr=subprocess.PIPE
+		)
 
-        print('Compilation result: ', compiler_result.returncode)
-        if compiler_result.returncode != 0:
-            # Failed compilation, return error message
-            return compiler_result.stderr.decode()
-        
-        # Run the compiled Java code
-        run_result = subprocess.run(
-            ['java', '-classpath', '/tmp', 'Main'], stdout=subprocess.PIPE, stderr=subprocess.PIPE
-        )
+		print('Compilation result: ', compiler_result.returncode)
+		if compiler_result.returncode != 0:
+			# Failed compilation, return error message
+			return compiler_result.stderr.decode()
+		
+		# Run the compiled Java code
+		run_result = subprocess.run(
+			['java', '-classpath', '/tmp', 'Main'], stdout=subprocess.PIPE, stderr=subprocess.PIPE
+		)
 
-        print('Run result: ', run_result.returncode)
-        return run_result.stdout.decode()
-    except Exception as e:
-        return str(e)
+		print('Run result: ', run_result.returncode)
+		return run_result.stdout.decode()
+	except Exception as e:
+		return str(e)
 
 
 def execute_cpp(code):
-    try:
-        print('Received code:\n', code)
+	try:
+		print('Received code:\n', code)
 
-        # Create temp c++ file
-        with open('/tmp/temp.cpp', 'w') as cpp_file:
-            cpp_file.write(code)
-        
-        # Compile c++ file
-        compile_result = subprocess.run(
-            ['g++', '/tmp/temp.cpp', '-o', '/tmp/temp'],
-            stdout=subprocess.PIPE, stderr=subprocess.PIPE
-        )
+		# Create temp c++ file
+		with open('/tmp/temp.cpp', 'w') as cpp_file:
+			cpp_file.write(code)
+		
+		# Compile c++ file
+		compile_result = subprocess.run(
+			['g++', '/tmp/temp.cpp', '-o', '/tmp/temp'],
+			stdout=subprocess.PIPE, stderr=subprocess.PIPE
+		)
 
-        print('Compilation result: ', compile_result.returncode)
-        if compile_result.returncode != 0:
-            # Failed compilation, return error
-            return compile_result.stderr.decode()
-        
-        # Run compiled c++ code
-        run_result = subprocess.run(
-            ['/tmp/temp'],
-            stdout=subprocess.PIPE, stderr=subprocess.PIPE
-        )
+		print('Compilation result: ', compile_result.returncode)
+		if compile_result.returncode != 0:
+			# Failed compilation, return error
+			return compile_result.stderr.decode()
+		
+		# Run compiled c++ code
+		run_result = subprocess.run(
+			['/tmp/temp'],
+			stdout=subprocess.PIPE, stderr=subprocess.PIPE
+		)
 
-        print('Run result:', run_result.returncode)
-        return run_result.stdout.decode()
-    except Exception as e:
-        return str(e)
+		print('Run result:', run_result.returncode)
+		return run_result.stdout.decode()
+	except Exception as e:
+		return str(e)
 
 
 def handler(event, context):
-    # Inputs from JSON
-    language = event.get('language', 'python')
-    code = event.get('code', '')
+	# Inputs from JSON
+	language = event.get('language', 'python')
+	code = event.get('code', '')
 
-    if language == 'python':
-        result = execute_python(code)
-    elif language == 'java':
-        result = execute_java(code)
-    elif language == 'cpp':
-        result = execute_cpp(code)
-    else:
-        result = 'Unsupported Language'
-    
-    return {
-        'status': 200,
-        'body': result
-    }
+	if language == 'python':
+		result = execute_python(code)
+	elif language == 'java':
+		result = execute_java(code)
+	elif language == 'cpp':
+		result = execute_cpp(code)
+	else:
+		result = 'Unsupported Language'
+	
+	return {
+		"statusCode": 200,
+		"headers": {
+        	"Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers": "Content-Type",
+            "Access-Control-Allow-Methods": "OPTIONS,POST"
+        },
+		"body": result
+	}
--- compiler-app/src/App.js
+++ compiler-app/src/App.js
@@ -5,53 +5,70 @@ import { useEffect, useState } from 'react';
 
 function App() {
 	const [code, setCode] = useState("");
-	const [language, setLanguage] = useState("");
+	const [language, setLanguage] = useState("python"); // Default language
 
-  const submitCode = () => {
-    // API logic goes in here
-    console.log(code);
-  };
+	const submitCode = async () => {
+		// API logic goes in here
+		console.log(code);
+		try{
+			const answer = await fetch("https://1gah1dspf6.execute-api.us-east-1.amazonaws.com/production",{
+				headers: {
+					"Content-Type": "application/json",
 
-  // useEffect(() => {
-  //   console.log(code);
-  // }, [code])
+				},
+				method: "POST",
+				body: JSON.stringify({
+					code: code,
+					language: language
+			})
+			});
+
+			const response = await answer.json();
+
+			console.log(response);
+		}
+		catch (e)
+		{
+			console.log(e.message);
+		}
+  	};
 
 	return (
 		<div className="App">
-      <button style={{background:language === 'python' ? 'black' : 'white', color:language === 'python' ? 'white' : 'black'}} onClick={() => {
-        setLanguage("python");
-      }}>
-        Python
-      </button>
+	  	<button style={{background:language === 'python' ? 'black' : 'white', color:language === 'python' ? 'white' : 'black'}} onClick={() => {
+			setLanguage("python");
+	  	}}>
+		Python
+	  	</button>
 
-      <button style={{background:language === 'java' ? 'black' : 'white', color:language === 'java' ? 'white' : 'black'}} onClick={() => {
-        setLanguage("java");
-      }}>
-        Java
-      </button>
+	  	<button style={{background:language === 'java' ? 'black' : 'white', color:language === 'java' ? 'white' : 'black'}} onClick={() => {
+			setLanguage("java");
+	  	}}>
+		Java
+	  	</button>
 
-      <button style={{background:language === 'cpp' ? 'black' : 'white', color:language === 'cpp' ? 'white' : 'black'}} onClick={() => {
-        setLanguage("cpp");
-      }}>
-        C++
-      </button>
+	  	<button style={{background:language === 'cpp' ? 'black' : 'white', color:language === 'cpp' ? 'white' : 'black'}} onClick={() => {
+			setLanguage("cpp");
+	  	}}>
+		C++
+	  	</button>
 
-      <button onClick={() => {
-        submitCode();
-      }}>
-        Compile
-      </button>
+	  	<button onClick={() => {
+			submitCode();
+	  	}}>
+		Compile
+	  	</button>
 
-			<Editor 
-        onChange={(e) => {
-            setCode(e);
-        }}
-        value={code}
-        theme="vs-dark"
-				height="90vh" 
-				defaultLanguage="python" 
-				defaultValue="// some comment" 
-			/>
+		<Editor 
+			onChange={(e) => {
+				setCode(e);
+			}}
+			value={code}
+			theme="vs-dark"
+			height="90vh" 
+			defaultLanguage="python" 
+			defaultValue="# Start Coding" 
+		/>
 		</div>
 	);
 }
